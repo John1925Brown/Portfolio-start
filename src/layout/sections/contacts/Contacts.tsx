e5a76169ab32afbb0bea0ae5ebe2a001504c8c56
Dzmitry Yarasheuski <materik08082020@gmail.com>
2024-11-18 22:50:55 +0300

    add form api to send messages

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -2,19 +2,46 @@ import { Container } from '../../../components/Container';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Button } from '../../../components/Button';
-import React from 'react';
+import React, { ElementRef, useRef } from 'react';
 import { S } from './Contacts_Styles';
+import emailjs from '@emailjs/browser';
 
 export const Contacts: React.FC = () => {
+  const form = useRef<ElementRef<'form'>>(null);
+
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    if (!form.current) return;
+
+    emailjs
+      .sendForm('service_hmzhemm', 'template_h5gz0x8', form.current, {
+        publicKey: 'nTCkW6lkQpeSLDNcU',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
     <section id={'contact'}>
       <Container>
         <SectionTitle title="Contacts" />
-        <S.Form action="#">
+        <S.Form ref={form} onSubmit={sendEmail}>
           <FlexWrapper direction="column" gap="15px">
-            <S.Field placeholder="Name" />
-            <S.Field placeholder="Email" />
-            <S.Field placeholder="Message" as={'textarea'}></S.Field>
+            <S.Field required placeholder="Name" name={'user_name'} />
+            <S.Field required placeholder="Email" name={'email'} />
+            <S.Field
+              required
+              placeholder="Message"
+              as={'textarea'}
+              name={'message'}
+            ></S.Field>
             <Button type="submit">Send message</Button>
           </FlexWrapper>
         </S.Form>
